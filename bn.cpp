8e2ff2c8930df8b65de2fad19db76b4893b169ea
saurav-computer <70015725+saurav-computer@users.noreply.github.com>
2020-08-21 23:57:32 +1200

    Add files via upload

---
+++ bn.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+//#include<conio.h>
+using namespace std;
+ int main()
+ {
+ 	int n;
+ 	cout<<"enter the value n";
+ 	cin>>n;
+
+ 	if (n>50)
+ 	{
+ 		cout<<"if condition is satifid " << endl;
+
+ 		cout<<n<<"  is greater then 50"<<endl;
+ 	}
+ 	else if(n<50 )
+ 	{
+ 		cout<<" comdition is not satified"<<endl;
+ 		cout<<n<<" is not greater then 50"<<endl;
+
+
+ 	}
+//cout<<"value of is :"<<n<<endl;
+ return 0;
+ 	
+} 
